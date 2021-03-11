ae22802f3c3aa56d6d8a7a70623e4567dc920f99
eduqg <eduardoqgomes@gmail.com>
2021-03-11 16:36:25 -0300

    Show stripe skus and products

---
+++ nextjs-stripe-payment/pages/[skuId].tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Stripe from 'stripe';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import Router from 'next/router';
+
+import stripeConfig from '../config/stripe'
+
+interface SkuProps {
+  sku: Stripe.Sku
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: '2020-08-27'
+  });
+
+  // sku = estoque
+  const skus = await stripe.skus.list();
+
+  console.log(skus);
+  const paths = skus.data.map(sku => ({
+    params: {
+      skuId: sku.id
+    }
+  }))
+
+  console.log(paths)
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: '2020-08-27'
+  });
+
+  const { skuId } = params;
+
+  const sku = await stripe.skus.retrieve(skuId as string);
+
+  return {
+    props: {
+      sku
+    }
+  }
+}
+
+const Product: React.FC<SkuProps> = ({ sku }) => {
+  return (
+    <div>
+      <h1>Product {sku.attributes.name}</h1>
+
+
+      {sku.image && (
+        <img src={sku.image}
+          style={{ width: '100px' }} />
+      )}
+
+      <h2>{Number(sku.price / 100).toFixed(2)} {sku.currency.toUpperCase()}</h2>
+
+      <Link href="/">Go back</Link>
+    </div>
+  );
+}
+
+export default Product;
--- nextjs-stripe-payment/pages/index.tsx
+++ nextjs-stripe-payment/pages/index.tsx
@@ -1,8 +1,60 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-   <div>Simple Stripe Store</div>
-  )
+
+
+
+import React from 'react';
+import Stripe from 'stripe';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import Router from 'next/router';
+
+import stripeConfig from '../config/stripe'
+
+interface HomeProps {
+  skus: Stripe.Sku[]
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: '2020-08-27'
+  });
+
+  const skus = await stripe.skus.list();
+
+  return {
+    props: {
+      skus: skus.data
+    }
+  }
+}
+
+const Home: React.FC<HomeProps> = ({ skus }) => {
+  return (
+    <div>
+      <h1>Simple Stripe Storage</h1>
+
+      <hr />
+
+      {skus.map(sku => (
+        <div key={sku.id}>
+          <h1>Product {sku.attributes.name}</h1>
+
+
+          {sku.image && (
+            <img src={sku.image}
+              style={{ width: '100px' }} />
+          )}
+
+          <h2>{Number(sku.price / 100).toFixed(2)} {sku.currency.toUpperCase()}</h2>
+
+          <Link href={"/" + sku.id}>Show product</Link>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
